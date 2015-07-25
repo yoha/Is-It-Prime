a88337bcac515c7f727bd550dade091bac9493b8
Yohannes Wijaya <yohannes.wijaya@gmail.com>
2015-07-25 13:03:51 +0700

    Added: app's icon

--- Is It Prime/ViewController.swift
+++ Is It Prime/ViewController.swift
@@ -9,6 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    @IBOutlet weak var userInputTextField: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBAction func findOutButton(sender: UIButton) {
+        if let userEntryInt = Int(userInputTextField.text!) {
+            var isPrime = true
+            if userEntryInt == 1 || userEntryInt == 0 { isPrime = false }
+            if userEntryInt != 2 && userEntryInt != 1 {
+                for var divisor = 2; divisor < userEntryInt; divisor++ {
+                    if userEntryInt % divisor == 0 { isPrime = false }
+                }
+            }
+            if isPrime { resultLabel.text = "\(userEntryInt) is a prime" }
+            else { resultLabel.text = "\(userEntryInt) is not a prime" }
+        }
+        else {
+            resultLabel.text = "Invalid entry detected"
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
